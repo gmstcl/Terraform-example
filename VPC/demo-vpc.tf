e87e3e53b562ffae67b93e5a955cfcef7683581b
gmstcl <acloudq@gmail.com>
2024-11-21 22:45:55 +0900

    init

--- VPC/demo-vpc.tf
+++ VPC/demo-vpc.tf
@@ -13,7 +13,7 @@ module "vpc" {
   one_nat_gateway_per_az = true  
 
   # Add the 'map_public_ip_on_launch' setting for public subnets
-  map_public_ip_on_launch = true  # Enable auto-assign public IPv4
+#  map_public_ip_on_launch = true  # Enable auto-assign public IPv4
 
   tags = {
     Terraform    = "true"
@@ -33,14 +33,14 @@ resource "aws_ec2_tag" "private_subnet_b_tag" {
   value          = "demo-pvt-sn-1b"
 }
 
-# resource "aws_ec2_tag" "public_subnet_a_tag" {
-#   resource_id    = module.vpc.public_subnets[0]
-#   key            = "Name"
-#   value          = "demo-pub-sn-1a"
-# }
-# 
-# resource "aws_ec2_tag" "public_subnet_b_tag" {
-#   resource_id    = module.vpc.public_subnets[1]
-#   key            = "Name"
-#   value          = "demo-pub-sn-1b"
-# }
+resource "aws_ec2_tag" "public_subnet_a_tag" {
+  resource_id    = module.vpc.public_subnets[0]
+  key            = "Name"
+  value          = "demo-pub-sn-1a"
+}
+
+resource "aws_ec2_tag" "public_subnet_b_tag" {
+  resource_id    = module.vpc.public_subnets[1]
+  key            = "Name"
+  value          = "demo-pub-sn-1b"
+}
